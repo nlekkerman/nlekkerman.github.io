d11fef15140a4a751c516b8de2566df7f1c9817a
nikola simic <nlekkerman@gmail.com>
2024-12-06 16:03:24 +0000

    Fixed links for deployment.

--- nikola-simic-portfolio/vite.config.js
+++ nikola-simic-portfolio/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nlekkerman.github.io/',
+  base: '/',
   server: {
     host: '0.0.0.0',  // Listens on all network interfaces (useful for local testing)
     port: 5173,        // Make sure the port is correct
