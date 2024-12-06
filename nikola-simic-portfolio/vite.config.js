dca77d2ea18b9617c66dc23742b8be20fec8a486
nikola simic <nlekkerman@gmail.com>
2024-12-06 15:43:32 +0000

    Moved images to public

--- nikola-simic-portfolio/vite.config.js
+++ nikola-simic-portfolio/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // For 'nlekkerman.github.io', this ensures correct asset paths
+  base: '/nlekkerman.github.io/',
   server: {
     host: '0.0.0.0',  // Listens on all network interfaces (useful for local testing)
     port: 5173,        // Make sure the port is correct
